c0fdce75847a5471ee1681fadc3fbcf4b76b1b7e
baron <richstyles@gmail.com>
2010-10-31 13:42:46 +0900

    add java mvn etc

--- rebuildlibs.sh
+++ rebuildlibs.sh
@@ -5,3 +5,5 @@ cd $eed/bbdb*/lisp && make
 find $eed/site-lisp -type f -name *.el -print0 | xargs -0 emacs -batch -f batch-byte-compile
 find $eed/elpa/ -type f -name *.el -print0 | xargs -0 emacs -batch -f batch-byte-compile
 find $eed/reftex*/lisp -type f -name *.el -print0 | xargs -0 emacs -batch -f batch-byte-compile
+find $eed/elib* -type f -name *.el -print0 | xargs -0 emacs -batch -f batch-byte-compile
+find $eed/jdee-*/lisp -type f -name *.el -print0 | xargs -0 emacs -batch -f batch-byte-compile
