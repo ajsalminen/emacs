ab9ff9d71dbe40bb774a24e84fd2e11a969c691c
baron <richstyles@gmail.com>
2010-10-31 14:43:46 +0900

    allow arbitrary commands in mvn, added jetty default

--- .emacs.d/site-lisp/mvn.el
+++ .emacs.d/site-lisp/mvn.el
@@ -9,7 +9,7 @@
   '("^\\[WARNING\\] \\([a-zA-Z0-9/-]+[.]scala\\):\\([0-9]+\\): error:" 1 2 nil 2 nil))
 
 (defun mvn-compile-buffer-name (mode)
- "*Maven Compile*")
+  "*Maven Compile*")
 
 (set 'scala-compile-error-regex  '("^\\[WARNING\\] \\([a-zA-Z0-9/-]+[.]scala\\):\\([0-9]+\\): error:" 1 2 nil 2 nil))
 
@@ -18,36 +18,36 @@
 
 (set 'compilation-mode-font-lock-keywords
      '(("^\\[ERROR\\] BUILD FAILURE"
-	(0 compilation-error-face))
+        (0 compilation-error-face))
        ("^\\[WARNING\\]"
-	(0 compilation-warning-face))
+        (0 compilation-warning-face))
        ("^\\(\\[INFO\\]\\)\\([^\n]*\\)"
-	(1 compilation-info-face)
-	(2 compilation-line-face))
+        (1 compilation-info-face)
+        (2 compilation-line-face))
        ("^\\[ERROR\\]"
-	(0 compilation-error-face))
+        (0 compilation-error-face))
        ("\\(Failures\\): [1-9][0-9]*,"
-	(0 compilation-error-face))
+        (0 compilation-error-face))
        ("\\(Failures\\): 0"
-	(0 compilation-info-face))
+        (0 compilation-info-face))
        ("\\(Errors\\): [1-9][0-9]*,"
-	(0 compilation-error-face))
+        (0 compilation-error-face))
        ("\\(Errors\\): 0"
-	(0 compilation-info-face))
+        (0 compilation-info-face))
        ("\\(Skipped\\): [1-9][0-9]*,"
-	(0 compilation-error-face))
+        (0 compilation-error-face))
        ("\\(Skipped\\): 0"
-	(0 compilation-info-face))
+        (0 compilation-info-face))
        ("Tests run: [0-9]+"
-	(0 compilation-warning-face))
+        (0 compilation-warning-face))
        ("T E S T S"
-	(0 compilation-warning-face))
+        (0 compilation-warning-face))
        ("Compilation finished at [^\n]+"
-	(0 compilation-info-face))
+        (0 compilation-info-face))
        ("^Compilation \\(exited abnormally\\|interrupt\\|killed\\|terminated\\|segmentation fault\\)\\(?:.*with code \\([0-9]+\\)\\)?.*"
-	(0 '(face nil message nil help-echo nil mouse-face nil) t)
-	(1 compilation-error-face)
-	(2 compilation-error-face nil t))))
+        (0 '(face nil message nil help-echo nil mouse-face nil) t)
+        (1 compilation-error-face)
+        (2 compilation-error-face nil t))))
 
 (defun mvn-at-root (path)
   "Determine if the given path is root."
@@ -66,8 +66,8 @@
   (let ((fn (buffer-file-name)))
     (let ((path (file-name-directory fn)))
       (while (and (not (mvn-pom-at-path-p path))
-		  (not (mvn-at-root path)))
-	(setf path (file-truename (mvn-parent-path path))))
+                  (not (mvn-at-root path)))
+        (setf path (file-truename (mvn-parent-path path))))
       path)))
 
 (defun mvn-master (&optional args)
@@ -76,28 +76,30 @@
   (let ((pom-path (mvn-find-path-to-pom)))
     (let ((parent-pom-path (file-truename (concat pom-path "/.."))))
       (if (not (mvn-pom-at-path-p parent-pom-path))
-	  (message "No master pom found.")
-	  (compile (read-from-minibuffer "Command: "
-					 (concat "mvn -o -f " parent-pom-path "/pom.xml clean install")
-					 nil nil 'mvn-command-history))))))
+          (message "No master pom found.")
+        (compile (read-from-minibuffer "Command: "
+                                       (concat "mvn -o -f " parent-pom-path "/pom.xml clean install")
+                                       nil nil 'mvn-command-history))))))
 
 (defun mvn (cmd)
   "Runs maven in the current project. Starting at the directoy where the file being vsisited resides, a search is
    made for pom.xml recsurively. A maven command is made from the first directory where the pom.xml file is found is then displayed
   in the minibuffer. The command can be edited as needed and then executed. Errors are navigate to as in any other compile mode"
-  (interactive "n0-clean 1-install 2-install,bundle:install 3-compile 4-test : ")
+  (interactive "s1-clean 2-install 3-install,bundle:install 4-compile 5-test 6-jetty:run: ")
   (let ((path (mvn-find-path-to-pom)))
-    (let ((goal (case cmd
-		  ((0) "clean")
-		  ((1) "install")
-		  ((2) "install bundle:install")
-		  ((3) "compile")
-		  ((4) "test"))))
+    (let ((goal (case (string-to-number cmd)
+                  ((1) "clean")
+                  ((2) "install")
+                  ((3) "install bundle:install")
+                  ((4) "compile")
+                  ((5) "test")
+                  ((6) "jetty:run")
+                  ((0) cmd))))
       (if (not (file-exists-p (concat path "/pom.xml")))
-	  (message "No pom.xml found")
-	(compile (read-from-minibuffer "Command: "
-				       (concat "mvn -o -f " path (concat "/pom.xml " goal))
-				       nil nil 'mvn-command-history))))))
+          (message "No pom.xml found")
+        (compile (read-from-minibuffer "Command: "
+                                       (concat "mvn -o -f " path (concat "/pom.xml " goal))
+                                       nil nil 'mvn-command-history))))))
 
 ;; String pattern for locating errors in maven output. This assumes a Windows drive letter at the beginning
 ;;(add-to-list
