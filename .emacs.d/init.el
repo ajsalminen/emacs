a4906c2a29ec95537ad8873f033ef122737e0227
baron <richstyles@gmail.com>
2010-12-04 15:57:03 +0900

    make iswitchb navigation gentler

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -611,6 +611,13 @@
 (setq column-number-mode t)
 
 (iswitchb-mode 1)
+(add-hook 'iswitchb-define-mode-map-hook
+      (lambda ()
+        (define-key iswitchb-mode-map "\C-n" 'iswitchb-next-match)
+        (define-key iswitchb-mode-map "\C-p" 'iswitchb-prev-match)
+        (define-key iswitchb-mode-map "\C-f" 'iswitchb-next-match)
+        (define-key iswitchb-mode-map "\C-b" 'iswitchb-prev-match)))
+
 
 (file-cache-add-directory "~/projects/ghub")
 
