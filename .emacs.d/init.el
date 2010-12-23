ac1aacd3d6e4262b458359a1374bf1c2c6d03cc3
baron <richstyles@gmail.com>
2010-12-23 12:57:31 +0900

    resume window stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -616,6 +616,37 @@
 (setq column-number-mode t)
 
 (iswitchb-mode 1)
+
+(require 'filecache)
+
+(defun file-cache-iswitchb-file ()
+  "Using iswitchb, interactively open file from file cache'.
+First select a file, matched using iswitchb against the contents
+in `file-cache-alist'. If the file exist in more than one
+directory, select directory. Lastly the file is opened."
+  (interactive)
+  (let* ((file (file-cache-iswitchb-read "File: "
+                                         (mapcar
+                                          (lambda (x)
+                                            (car x))
+                                          file-cache-alist)))
+         (record (assoc file file-cache-alist)))
+    (find-file
+     (concat
+      (if (= (length record) 2)
+          (car (cdr record))
+        (file-cache-iswitchb-read
+         (format "Find %s in dir: " file) (cdr record))) file))))
+
+(defun file-cache-iswitchb-read (prompt choices)
+  (let ((iswitchb-make-buflist-hook
+	 (lambda ()
+	   (setq iswitchb-temp-buflist choices))))
+    (iswitchb-read-buffer prompt)))
+
+(global-set-key "\C-cf" 'file-cache-iswitchb-file)
+
+
 (add-hook 'iswitchb-define-mode-map-hook
       (lambda ()
         (define-key iswitchb-mode-map "\C-n" 'iswitchb-next-match)
@@ -623,8 +654,14 @@
         (define-key iswitchb-mode-map "\C-f" 'iswitchb-next-match)
         (define-key iswitchb-mode-map "\C-b" 'iswitchb-prev-match)))
 
-
 (file-cache-add-directory "~/projects/ghub")
+
+(defun file-cache-add-this-file ()
+  (and buffer-file-name
+       (file-exists-p buffer-file-name)
+       (file-cache-add-file buffer-file-name)))
+(add-hook 'kill-buffer-hook 'file-cache-add-this-file)
+
 
 ;; File Name Cache
 ;; http://www.emacswiki.org/emacs/FileNameCache
@@ -632,7 +669,6 @@
     "filecache"
   '(progn
      (message "Loading file cache...")
-     ;;     (file-cache-add-directory-using-find "~/projects")
      (file-cache-add-directory-list load-path)))
 
 
@@ -753,7 +789,7 @@
 
 (require 'deferred)
 
-(defun async-org-mobile-pullpussh
+(defun async-org-mobile-pullpush
   (deferred:call(org-mobile-pullpush)))
 
 (run-at-time t 3600 'async-org-mobile-pullpush)
@@ -1201,7 +1237,8 @@
 (auto-install-compatibility-setup)
 
 (require 'auto-async-byte-compile)
-(setq auto-async-byte-compile-exclude-files-regexp "/org/")
+(setq auto-async-byte-compile-exclude-files-regexp "org\\|junk\\|\\.revive\\.el")
+
 (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
 
 (require 'uniquify)
@@ -1533,6 +1570,14 @@
 
 (require 'window-numbering)
 (window-numbering-mode 1)
+
+(autoload 'save-current-configuration "revive" "Save status" t)
+(autoload 'resume "revive" "Resume Emacs" t)
+(autoload 'wipe "revive" "Wipe Emacs" t)
+
+(setq revive:major-mode-command-alist-private
+      '((w3m-mode . w3m)
+        ("*w3m*" . w3m)))
 
 (server-start)
 
