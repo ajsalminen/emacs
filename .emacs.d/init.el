cc451d935c642759484f40027a3f812b8f711555
baron <richstyles@gmail.com>
2011-02-10 15:26:32 +0900

    added auto mark

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1863,6 +1863,21 @@ post command hook に機能追加"
 
 (defalias 'ti 'echo-time-now)
 
+(require 'bm)
+
+(when (require 'auto-mark nil t)
+  (setq auto-mark-command-class-alist
+        '((anything . anything)
+          (goto-line . jump)
+          (indent-for-tab-command . ignore)
+          (undo . ignore)))
+  (setq auto-mark-command-classifiers
+        (list (lambda (command)
+                (if (and (eq command 'self-insert-command)
+                         (eq last-command-char ? ))
+                    'ignore))))
+  (global-auto-mark-mode 1))
+
 (server-start)
 
 (message "********** successfully initialized **********")
