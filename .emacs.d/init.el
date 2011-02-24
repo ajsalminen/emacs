3aa1dbadb07768e702a9c5b2b2ebb4ef24f19219
baron <richstyles@gmail.com>
2011-02-25 08:58:14 +0900

    all kinds of sudos oh my

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2037,6 +2037,49 @@ post command hook に機能追加"
 
 (defalias 'ts 'toggle-line-spacing)
 
+(defun sudo-edit (&optional arg)
+  (interactive "p")
+  (if arg
+      (find-file (concat "/sudo:root@localhost:" (ido-read-file-name "File: ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+(defun sudo-edit-current-file ()
+  (interactive)
+  (find-alternate-file (concat "/sudo:root@localhost:" (buffer-file-name (current-buffer)))))
+
+;; ;; code from failed sudo attempt
+;; (require 'sudo)
+
+;; (defun sudo-before-save-hook ()
+;;   (set (make-local-variable 'sudo:file) (buffer-file-name))
+;;   (when sudo:file
+;;     (unless(file-writable-p sudo:file)
+;;       (set (make-local-variable 'sudo:old-owner-uid) (nth 2 (file-attributes sudo:file)))
+;;       (when (numberp sudo:old-owner-uid)
+;; 	(unless (= (user-uid) sudo:old-owner-uid)
+;; 	  (when (y-or-n-p
+;; 		 (format "File %s is owned by %s, save it with sudo? "
+;; 			 (file-name-nondirectory sudo:file)
+;; 			 (user-login-name sudo:old-owner-uid)))
+;; 	    (sudo-chown-file (int-to-string (user-uid)) (sudo-quoting sudo:file))
+;; 	    (add-hook 'after-save-hook
+;; 		      (lambda ()
+;; 			(sudo-chown-file (int-to-string sudo:old-owner-uid)
+;; 					 (sudo-quoting sudo:file))
+;; 			(if sudo-clear-password-always
+;; 			    (sudo-kill-password-timeout)))
+;; 		      nil ;; not append
+;; 		      t   ;; buffer local hook
+;; 		      )))))))
+
+
+;; (add-hook 'before-save-hook 'sudo-before-save-hook)
+
+
+
 (server-start)
+;; sudo-ext requires server
+(when (and (= emacs-major-version 23) (eq window-system 'x))
+  (require 'sudo-ext))
 
 (message "********** successfully initialized **********")
