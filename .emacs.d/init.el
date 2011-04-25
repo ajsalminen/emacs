b7db826e9d70dbe6a73f08ec1d96214aa2b9eb56
sb <richstyles@gmail.com>
2011-04-25 15:31:08 +0900

    just require drupal mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3191,11 +3191,12 @@ FORMAT-STRING is like `format', but it can have multiple %-sequences."
           '(lambda ()
              (setq php-manual-path "~/Dropbox/php_manual/")))
 
+(require 'drupal)
+(autoload 'drupal-mode "drupal-mode" "Major mode for editing drupal php " t)
+
 (add-to-list 'auto-mode-alist '("\\.\\(module\\|test\\|install\\|theme\\)$" . drupal-mode))
 (add-to-list 'auto-mode-alist '("\\.\\(php\\|inc\\)$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.info" . conf-windows-mode))
-
-(autoload 'drupal-mode "drupal-mode" "Major mode for editing drupal php " t)
 
 (require 'php-doc nil t)
 (setq php-doc-directory "~/Dropbox/php_manual/")
