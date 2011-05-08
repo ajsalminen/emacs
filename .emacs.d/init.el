c38dbd8fe598ae8a89ae124a79ff10dd7a7fb90f
baron <richstyles@gmail.com>
2011-05-09 06:32:37 +0900

    supress warning info

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -445,6 +445,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+
+ '(warning-suppress-types (quote ((undo discard-info))))
  '(inhibit-startup-screen t)
  '(inhibit-startup-message t))
 (custom-set-faces
