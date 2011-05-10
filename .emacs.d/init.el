c3092a92aab0e76dc818d456c8d1355f2f34e837
sb <richstyles@gmail.com>
2011-05-10 15:01:32 +0900

    add line to init for dabbrev

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3331,4 +3331,6 @@ FORMAT-STRING is like `format', but it can have multiple %-sequences."
 (require 'whitespace)
 (setq whitespace-display-mappings '((space-mark ?\  [?\u00B7]) (newline-mark ?\n [?$ ?\n]) (tab-mark ?\t [?\u00BB ?\t])))
 
+(require 'dabbrev-ja)
+
 (message "********** successfully initialized **********")
