f5a9c221563294feaaa299bb46ccf2c1b855c196
baron <richstyles@gmail.com>
2011-05-15 21:41:32 +0900

    would you like some ruby with that?

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,6 +30,7 @@
                         "~/.emacs.d/org-mode/lisp"
                         "~/.emacs.d/reftex-4.34a/lisp"
                         "~/.emacs.d/rhtml"
+                        "~/.emacs.d/rinari"
                         "~/.emacs.d/site-lisp"
                         "~/.emacs.d/skype"
                         "~/.emacs.d/twittering"
@@ -2408,8 +2409,59 @@ directory, select directory. Lastly the file is opened."
 (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
 (require 'dired-sort-map)
 
+(require 'rinari)
 (require 'rhtml-mode)
 (require 'ruby-electric)
+(require 'textmate)
+(define-key *textmate-mode-map* (kbd "M-;") 'comment-or-uncomment-region-or-line)
+
+(defun is-rails-project ()
+  (when (textmate-project-root)
+    (file-exists-p (expand-file-name "config/environment.rb" (textmate-project-root)))))
+
+(defun run-rails-test-or-ruby-buffer ()
+  (interactive)
+  (if (is-rails-project)
+      (let* ((path (buffer-file-name))
+             (filename (file-name-nondirectory path))
+             (test-path (expand-file-name "test" (textmate-project-root)))
+             (command (list ruby-compilation-executable "-I" test-path path)))
+        (pop-to-buffer (ruby-compilation-do filename command)))
+    (ruby-compilation-this-buffer)))
+
+
+(autoload 'ruby-mode "ruby-mode" nil t)
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
+(add-hook 'ruby-mode-hook '(lambda ()
+                             (setq ruby-deep-arglist t)
+                             (setq ruby-deep-indent-paren nil)
+                             (setq c-tab-always-indent nil)
+                             (require 'inf-ruby)
+                             (require 'ruby-compilation)
+                             (define-key ruby-mode-map (kbd "M-r") 'run-rails-test-or-ruby-buffer)))
+
+(autoload 'rhtml-mode "rhtml-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . rhtml-mode))
+(add-to-list 'auto-mode-alist '("\\.rjs\\'" . rhtml-mode))
+(add-hook 'rhtml-mode '(lambda ()
+                         (define-key rhtml-mode-map (kbd "M-s") 'save-buffer)))
+
+(autoload 'yaml-mode "yaml-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+
+(autoload 'css-mode "css-mode")
+(setq auto-mode-alist
+      (cons '("\\.css\\'" . css-mode) auto-mode-alist))
+(setq cssm-indent-function #'cssm-c-style-indenter)
+(add-hook 'css-mode-hook '(lambda ()
+                            (setq css-indent-level 2)
+                            (setq css-indent-offset 2)))
 
 (require 'sense-region)
 (sense-region-on)
@@ -3322,11 +3374,6 @@ FORMAT-STRING is like `format', but it can have multiple %-sequences."
             (set (make-local-variable 'eldoc-documentation-function)
                  'php-doc-eldoc-function)
             (eldoc-mode 1)))
-
-(autoload 'css-mode "css-mode")
-(setq auto-mode-alist
-      (cons '("\\.css\\'" . css-mode) auto-mode-alist))
-(setq cssm-indent-function #'cssm-c-style-indenter)
 
 (require 'wide-n)
 
