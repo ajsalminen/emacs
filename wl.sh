612492fb2d8100128d89c94c78bbd8e200355516
baron <richstyles@gmail.com>
2011-05-22 18:31:00 +0900

    update wanderlust shell script

--- wl.sh
+++ wl.sh
@@ -20,19 +20,6 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
     lisplocal_string='--with-lispdir=~/emacs.d/site-lisp'
 fi
 
-cd $eed/org-mode
-make clean
-make --makefile=$eed/orgmakefile_mac
-make install --makefile=$eed/orgmakefile_mac
-
-cd $eed/cedet-1.0
-make clean
-make
-
-cd $eed/emacs-w3m
-autoconf
-make clean
-
 if [[ "$unamestr" == 'Linux' ]]; then
     ./configure
     make
@@ -72,34 +59,3 @@ cd $eed/el-get/wanderlust
 make clean
 make $install_string
 make install $install_string
-
-cd $eed/org-mode
-make clean
-make $install_string
-make install $install_string
-
-cd $eed/muse-3.20
-make clean
-make $install_string
-make install $install_string
-
-cd $eed/auto-complete-1.3.1
-make clean
-make $install_string
-
-cd $eed/scala
-make clean
-make
-
-for i in `ls $eed`; do cd $eed/$i && make; done;
-find $eed/site-lisp -type f -name *.el -print0 | xargs -0 emacs -batch -f batch-byte-compile
-find $eed/elpa/ -type f -name *.el -print0 | xargs -0 emacs -batch -f batch-byte-compile
-find $eed/haskell-mode/ -type f -name *.el -print0 | xargs -0 emacs -batch -f batch-byte-compile
-find $eed/reftex*/lisp -type f -name *.el -print0 | xargs -0 emacs -batch -f batch-byte-compile
-find $eed/elib* -type f -name *.el -print0 | xargs -0 emacs -batch -f batch-byte-compile
-find $eed/jdee-*/lisp -type f -name *.el -print0 | xargs -0 emacs -batch -f batch-byte-compile
-find $eed/ensime*/elisp -type f -name *.el -print0 | xargs -0 emacs -batch -f batch-byte-compile
-for i in `find $eed/* -type f -name *.el`; do emacs -batch -f batch-byte-compile $i; done;
-#cd $eed/bbdb-* && make
-#cd $eed/bbdb-*/lisp && make
-cd $eed/yatex* && make elc
