aee0ef9381a06b22437582cc49eb17e97173fad8
Sam Baron <richstyles@gmail.com>
2014-04-30 17:50:18 +0900

    add org mode packages

--- .emacs.d/inits/00-package-init.el
+++ .emacs.d/inits/00-package-init.el
@@ -9,6 +9,7 @@
 (setq package-enable-at-startup nil)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 ;; for package backports
