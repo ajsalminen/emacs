07520f917bda0b20fc751d6299205d95ec721cf6
Sam Baron <richstyles@gmail.com>
2014-12-14 16:35:30 +0900

    update packages

---
+++ .emacs.d/elpa/circe-20141212.241/circe-pkg.el
@@ -0,0 +1,6 @@
+(define-package "circe" "20141212.241" "Client for IRC in Emacs"
+  '((lui "0")
+    (lcs "0")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/malabar-mode-20141212.716/malabar-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; malabar-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "malabar-mode" "malabar-mode.el" (21645 13814
+;;;;;;  0 0))
+;;; Generated autoloads from malabar-mode.el
+
+(autoload 'malabar-mode "malabar-mode" "\
+Support and integeration for JVM languages
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("malabar-mode-pkg.el") (21645 13815 184297
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; malabar-mode-autoloads.el ends here
