ae6bb274fa70fd2b729160f8c9769ef0b0833c87
Sam Baron <richstyles@gmail.com>
2014-12-17 12:46:12 +0900

    refactor code for readability and saner formatting

--- .emacs.d/inits/99-azon.el
+++ .emacs.d/inits/99-azon.el
@@ -1,12 +1,28 @@
 (require 'request)
 
+;; takes vector of words and phrases which is then joined and split again
+;; to create an array of unique elements that can be rejoined into a string
+;; of keywords
+(defun azon-uniquify (vec)
+  (insert
+   (format "%s,\n"
+           (mapconcat 'identity
+                      (delete-dups
+                       (split-string
+                        (mapconcat 'identity
+                                   (append vec nil) " "))) " "))))
+
 (defun azon-keyword-lookup (kw)
   (interactive "sEnter keyword: ")
-  ;; (message "the kw: %s" ,kw)
   (request
    "http://completion.amazon.com/search/complete"
-   :params `(("method" . "completion") ("q" . ,kw) ("search-alias" . "digital-text") ("mkt" . "1"))
+   :params `(("method" . "completion")
+             ("q" . ,kw)
+             ("search-alias" . "digital-text")
+             ("mkt" . "1"))
    :parser 'json-read
    :success (function*
              (lambda (&key data &allow-other-keys)
-               (insert (format "%s,\n" (mapconcat 'identity (delete-dups (split-string (mapconcat 'identity (append (aref data 1) nil) " "))) " ")))))))
+               (azon-uniquify (aref data 1))))))
+
+
